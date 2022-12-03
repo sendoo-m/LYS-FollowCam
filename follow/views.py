41332cec8754f0a078cd17bf8aaa3e81c833708e
Sendoo <95181522+sendoo-m@users.noreply.github.com>
2022-12-03 09:32:44 +0200

    add follow report

--- follow/views.py
+++ follow/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 def add_dvr(request):
     
-    return render(request, 'form/dvr_form.html' , {})
+    return render(request, 'follow/dvr_form.html' , {})
 
 def all_dvr(request):
     
@@ -13,4 +13,10 @@ def all_dvr(request):
         'all_dvr1' :Add_DVR.objects.all(),
         'all_dvr2' :Building.objects.all()
         }
-    return render(request, 'form/all_dvr.html' , context)
+    return render(request, 'follow/all_dvr.html' , context)
+
+def follow_dvr(request):
+    context = {
+        
+        }
+    return render(request, 'follow/follow_dvr.html', context)
